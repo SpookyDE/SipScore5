b46d664f6f9d6089f871831a1eeb9891c05cc5c2
Moritz Brömme <moritz.broemme@gmail.com>
2024-02-27 16:41:28 +0100

    implemented navigation

--- app/src/main/java/com/sweng/sipscore5/AppActivity.kt
+++ app/src/main/java/com/sweng/sipscore5/AppActivity.kt
@@ -2,6 +2,9 @@ package com.sweng.sipscore5
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class AppActivity : AppCompatActivity() {
@@ -9,5 +12,12 @@ class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
     }
 }
--- app/src/main/java/com/sweng/sipscore5/database/UserDAO.kt
+++ app/src/main/java/com/sweng/sipscore5/database/UserDAO.kt
@@ -3,7 +3,6 @@ package com.sweng.sipscore5.database
 import com.sweng.sipscore5.models.User
 
 interface UserDAO {
-
     fun addUser (username : String, password : String)
 
     fun getUserByName (username : String) : User?
