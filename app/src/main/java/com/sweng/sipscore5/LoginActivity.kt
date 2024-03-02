283ebff27741bad3903bac23b9bc53f0757a9b44
Moritz Brömme <moritz.broemme@gmail.com>
2024-03-02 15:53:03 +0100

    implemented login session

--- app/src/main/java/com/sweng/sipscore5/LoginActivity.kt
+++ app/src/main/java/com/sweng/sipscore5/LoginActivity.kt
@@ -1,13 +1,12 @@
 package com.sweng.sipscore5
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.sweng.sipscore5.database.UserDAO
-import com.sweng.sipscore5.database.UserDAOImpl
 import com.sweng.sipscore5.models.User
 import com.sweng.sipscore5.services.UserService
 import com.sweng.sipscore5.services.UserServiceImpl
@@ -36,6 +35,10 @@ class LoginActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "Anmelden erfolgreich", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, AppActivity::class.java)
+                val sharedPref = getSharedPreferences("LoginPreferences", Context.MODE_PRIVATE)
+                val editor = sharedPref.edit()
+                editor.putString("username", user.username)
+                editor.apply()
                 startActivity(intent)
                 finish()
             }
